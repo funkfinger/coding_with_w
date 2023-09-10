b9789c51274d8e3365ce63bbd88e8dd1969c3dda
Jay Wiggins <5948+funkfinger@users.noreply.github.com>
2023-09-10 11:20:29 -0700

    first checkin

---
+++ Player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+@export var speed = 500
+# easy top down movment YAY! :^]
+func get_input(): # got the input to create the movement
+	var input_direction = Input.get_vector("left" , "right" , "up" , "down")
+	velocity = input_direction * speed
+func _physics_process(delta): #used built in process
+	get_input() 
+	move_and_slide() 
