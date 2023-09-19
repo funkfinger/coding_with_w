510acd6ea4cb9690a64808eeb67c58f3d5326852
Jay Wiggins <5948+funkfinger@users.noreply.github.com>
2023-09-19 13:54:40 -0700

    added tilemap collisions and camera

--- Player.gd
+++ Player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-@export var speed = 500
+@export var speed = 300
 # easy top down movment YAY! :^]
 func get_input(): # got the input to create the movement
 	var input_direction = Input.get_vector("left" , "right" , "up" , "down")
